739d11514e05cc08eee5b341989f00a4cf6559bd
vipul1435 <vipulkumarraj2003@gmail.com>
2023-08-16 02:58:06 +0530

    Websocket.io completed

---
+++ server/Actions.js
@@ -0,0 +1,9 @@
+const ACTIONS = {
+    JOIN:'join',
+    LEAVE:'leave',
+    JOINED:'joined',
+    DISCONNETED:'disconnected',
+    CODE_CHANGE:'code-change',
+    SYNC_CODE:'sync-code',
+}
+export default ACTIONS;
